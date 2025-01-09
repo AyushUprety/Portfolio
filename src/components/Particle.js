eeb68e5123c3b31dbbaa798d5f80464738796178
AyushUprety <ayushuprety1@gmail.com>
2025-01-09 18:02:10 -0500

    added particles

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -4,49 +4,23 @@ import Particles from "react-tsparticles";
 function Particle() {
   return (
     <Particles
-      id="tsparticles"
-      params={{
+      options={{
         particles: {
           number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 1500,
-            },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
+            value: 50,
           },
           size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
+            value: 3,
           },
         },
         interactivity: {
           events: {
-            onclick: {
+            onhover: {
               enable: true,
-              mode: "push",
-            },
-          },
-          modes: {
-            push: {
-              particles_nb: 1,
+              mode: "repulse",
             },
           },
         },
-        retina_detect: true,
       }}
     />
   );
